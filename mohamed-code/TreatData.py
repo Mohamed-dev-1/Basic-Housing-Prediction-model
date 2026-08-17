960fef6c6770717706f55d3c9e98a4e33bcdc215
mohamed dev <mouha02020@gmail.com>
2026-08-17 23:24:57 +0100

    load data and understand the data and clean the data

--- mohamed-code/TreatData.py
+++ mohamed-code/TreatData.py
@@ -1,1 +1,15 @@
 # load data here
+import pandas as pd
+
+def load_data():
+    df = pd.read_csv('dataset/housing.csv')
+    df = clean_data(df)
+    return df
+
+def clean_data(df):
+    df = df.fillna('')
+    return df
+
+df = load_data()
+print(df.isna().sum())
+
--- mohamed-code/main.py
+++ mohamed-code/main.py
@@ -1,2 +1,11 @@
 # main code here
 
+from TreatData import load_data
+
+df = load_data()
+
+# separate between features and target
+features = df.drop(['median_house_value'])
+
+# 
+target = df['median_house_value']
