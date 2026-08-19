649045ff4f2e41da30f9c5302c3dbe5fa26b6f9b
mohamed dev <mouha02020@gmail.com>
2026-08-20 00:39:19 +0100

    create the model object and fit the data into it

--- mohamed-code/TreatData.py
+++ mohamed-code/TreatData.py
@@ -12,6 +12,9 @@ def clean_data(df):
     # we found empty cells only in total_bedrooms column so i see that need to replace empty cells with the median value to ensure that the dtype stays float64 and not become an ibject when try to fill them with '' empty strings
     df['total_bedrooms'] = df['total_bedrooms'].fillna(df['total_bedrooms'].median())
     
+    # replace the ocean_proximity column with 5 -> set(values in this columns) columns each column 
+    df = pd.get_dummies(df, columns=['ocean_proximity'])
+    
     # no need to clean data from wrong format or data it looks good
     return df
 
--- mohamed-code/model.py
+++ mohamed-code/model.py
@@ -1,5 +1,7 @@
 from TreatData import load_data
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 # load the clean dataframe
 df = load_data()
@@ -15,3 +17,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     test_size=0.2,
     random_state=42
 )
+
+model = LinearRegression()
+model.fit(X_train, y_train)
